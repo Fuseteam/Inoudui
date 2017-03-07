35dd4d244b7221444ea780e97928618b7c8f39d6
RYArrowsmith <ryarrowsmith@gmail.com>
2017-03-07 23:04:31 +0000

    Dice command

---
+++ src/main/java/io/bfnt/relash/commands/random/Dice.java
@@ -0,0 +1,29 @@
+package io.bfnt.relash.commands.random;
+
+import io.bfnt.relash.util.RelashCommand;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.exceptions.PermissionException;
+
+import java.security.SecureRandom;
+
+/**
+ * Created by Ryan's PC on 07/03/2017.
+ */
+public class Dice extends RelashCommand {
+
+    public void dice(Message trigger){
+
+        final MessageChannel channel = trigger.getChannel();
+        final String result = "\uD83E\uDD23 Result: " + (new SecureRandom().nextInt(6) + 1);
+
+        try {
+
+            channel.sendMessage(makeEmbed(result, "").build()).queue();
+
+        } catch (PermissionException exception){
+
+            channel.sendMessage(result).queue();
+        }
+    }
+}
