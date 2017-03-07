3abc5deac6a696d5deaba4cedab6000c642e4e9e
RYArrowsmith <ryarrowsmith@gmail.com>
2017-03-07 22:21:06 +0000

    Adding cat command to help

--- src/main/java/io/bfnt/relash/commands/Help.java
+++ src/main/java/io/bfnt/relash/commands/Help.java
@@ -13,14 +13,15 @@ public class Help extends RelashCommand {
     public void help(Message trigger){
 
         final TextChannel channel = trigger.getTextChannel();
+        final String commands = "<~ cat - Returns a random cat.";
 
         try {
 
-            channel.sendMessage(makeEmbed("❗❗ Relash Help ❗❗","No commands yet.").build()).queue();
+            channel.sendMessage(makeEmbed("❗❗ Relash Help ❗❗","", "Commands: " + commands).build()).queue();
 
         } catch (PermissionException exception){
 
-            channel.sendMessage("❗❗ Relash Help ❗❗\nNo commands yet.").queue();
+            channel.sendMessage("❗❗ Relash Help ❗❗\n" + commands).queue();
         }
     }
 }
