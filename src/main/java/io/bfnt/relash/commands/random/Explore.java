ab9bae43a23162efce5fe972fd7b6d90028b6e75
RYArrowsmith <ryarrowsmith@gmail.com>
2017-03-11 21:45:58 +0000

    I WILL KILL MYSELF v4

--- src/main/java/io/bfnt/relash/commands/random/Explore.java
+++ src/main/java/io/bfnt/relash/commands/random/Explore.java
@@ -1,11 +1,14 @@
 package io.bfnt.relash.commands.random;
 
 import io.bfnt.relash.util.RelashCommand;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Invite;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.exceptions.PermissionException;
+
+import java.security.SecureRandom;
 
 /**
  * Created by Ryan's PC on 11/03/2017.
@@ -18,20 +21,48 @@ public class Explore extends RelashCommand {
         final String searching = "🔎 Searching...";
         final String found = "🔍 Found: ";
         final String link = "%s | https://discord.gg/%s";
-        final Invite invite = makeInvite(trigger.getGuild());
+        final JDA jda = trigger.getJDA();
+        makeInvite(jda.getGuilds().get(new SecureRandom().nextInt(jda.getGuilds().size())));
 
         try {
 
-            channel.sendMessage(makeEmbed(searching, "").build()).queue(search -> search.editMessage(makeEmbed(found, String.format(link, invite.getGuild().getName(), invite.getCode())).build()).queue());
+            channel.sendMessage(makeEmbed(searching, "").build()).queue(search -> {
+
+                Invite invite = null;
+
+                while (invite == null){
+
+                    invite = makeInvite(jda.getGuilds().get(new SecureRandom().nextInt(jda.getGuilds().size())));
+                }
+
+                search.editMessage(makeEmbed(found, String.format(link, invite.getGuild().getName(), invite.getCode())).build()).queue();
+            });
 
         } catch (PermissionException e){
 
-            channel.sendMessage(searching).queue(search -> search.editMessage(found + String.format(link, invite.getGuild(), invite.getCode())).queue());
+            channel.sendMessage(searching).queue(search -> {
+
+                Invite invite = null;
+
+                while (invite == null){
+
+                    invite = makeInvite(jda.getGuilds().get(new SecureRandom().nextInt(jda.getGuilds().size())));
+                }
+
+                search.editMessage(found + String.format(link, invite.getGuild(), invite.getCode())).queue();
+            });
         }
     }
 
     private Invite makeInvite(Guild guild){
 
-        return guild.getPublicChannel().createInvite().complete();
+        try {
+
+            return guild.getPublicChannel().createInvite().complete();
+
+        } catch (PermissionException e){
+
+            return null;
+        }
     }
 }
