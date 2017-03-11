b2f8b69da805a6652ef9a02d73412c0cefaa38e1
RYArrowsmith <ryarrowsmith@gmail.com>
2017-03-11 21:22:56 +0000

    I WILL KILL MYSELF v2

--- src/main/java/io/bfnt/relash/commands/random/Explore.java
+++ src/main/java/io/bfnt/relash/commands/random/Explore.java
@@ -1,28 +1,37 @@
 package io.bfnt.relash.commands.random;
 
-import net.dv8tion.jda.core.entities.Guild;
+import io.bfnt.relash.util.RelashCommand;
 import net.dv8tion.jda.core.entities.Invite;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.exceptions.PermissionException;
 
 /**
  * Created by Ryan's PC on 11/03/2017.
  */
-public class Explore {
+public class Explore extends RelashCommand {
 
-    public Invite getInvite(Guild guild){
+    public void explore(Message trigger){
 
-
-    }
-
-    public Invite createInvite(Guild guild){
+        final MessageChannel channel = trigger.getChannel();
+        final String searching = "🔎 Searching...";
+        final String found = "🔍 Found: ";
+        final String link = "%s | https://discord.gg/%s";
+        final Invite invite;
 
         try {
 
-            return guild.getPublicChannel().createInvite().complete();
+            channel.sendMessage(makeEmbed(searching, "").build()).queue(search -> {
+
+                search.editMessage(makeEmbed(found, String.format(link, invite.getGuild(), invite.getCode())).build()).queue();
+            });
 
         } catch (PermissionException e){
 
-            return null;
+            channel.sendMessage(searching).queue(search -> {
+
+                search.editMessage(found + String.format(link, invite.getGuild(), invite.getCode())).queue();
+            });
         }
     }
 }
